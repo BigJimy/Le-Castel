ccd224ee897d0b13c2c3c5fe49e385c5be8c7aaa
Jimy <jimy.bigaud85@laposte.net>
2018-03-27 16:12:30 +0200

    add room description and open hours

---
+++ assets/js/custom.js
@@ -0,0 +1,69 @@
+/* Add auto-padding to Header */
+$(document).ready(function() {
+    
+    'user strict';
+    
+    setInterval( function() {
+        
+        'user strict';
+        
+        var windowHeight = $(window).height();
+        var containerHeight = $(".header-container").height();
+        var padTop = windowHeight - containerHeight;
+        
+        $(".header-container").css({
+            
+            'padding-top': Math.round( padTop / 2) + 'px',
+            'padding-bottom': Math.round( padTop / 2) + 'px'
+        });
+    }, 10)
+      
+});
+
+/* Show and hide menu */
+
+$(document).ready(function(){
+    
+    'use strict';
+    
+    $(window).scroll(function() {
+        'use strict'; 
+        
+        if($(window).scrollTop() < 80 ) {
+            
+            $('.navbar').css({
+                'margin-top': '-100px',
+                'opacity': '0'
+            });
+            
+            $('.navbar-default').css({
+                'background-color': 'rgba( 0, 0, 0, 0)'
+            });
+            
+        } else {
+    
+            $('.navbar').css({
+                'margin-top': '0px',
+                'opacity': '1'
+            });
+            
+            $('.navbar-default').css({
+                'background-color': 'rgba(0, 0, 0, 0.8)',
+                'border-color': '#444'
+            });
+            
+            $('.navbar-brand img').css({
+//                'height': '35px',
+                'padding-top': 'Opx'
+                
+            });
+            
+            $('.navbar-nav > li > a').css({
+                'padding-top': '15px'
+                
+            });
+        }
+        
+    });
+    
+});
